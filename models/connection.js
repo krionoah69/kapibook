c1050be8f63353e5bc07b447ce81ebfae7009c0b
krionoah <rrojas@canteradigital.mx>
2018-04-24 00:29:48 -0500

    Coneccion BD

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,8 @@
 var connection = {
-    host: 'localhost',
-    user: 'monitorg_usrras',
-    password: 'KXRD00wjr9',
-    database: 'monitorg_rastreo'
+    host: 'mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'x0os5lk277hjb6rl',
+    password: 'odkhrs3mroo3nj9h',
+    database: 'q6yopera2c9j4x5e'
 }
 
 module.exports = connection;
