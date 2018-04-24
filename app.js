2bd29e5c34edf7c67ad2e2de95a8d3d83f646eb1
krionoah <rrojas@canteradigital.mx>
2018-04-24 10:33:46 -0500

    API 2.0

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-var express = require("express");
-var router = express.Router();
-var bodyParser  = require("body-parser");
-var aplicacion = express();
-var usuarios = require('./routes/rutasUsuario');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-router.get('/', function(request, response) {
-    response.status(200).json({"mensaje":"Nuestra primera app con node.js utilizando express"});
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-aplicacion.use(bodyParser.json());
-//incluimos el archivo en el que se almacenan las rutas de usuarios
-aplicacion.use(router);
-aplicacion.use(usuarios);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-aplicacion.listen(6969, function() {
-    console.log("Servidor iniciado");
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
--- models/connection.js
+++
@@ -1,8 +0,0 @@
-var connection = {
-    host: 'mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'x0os5lk277hjb6rl',
-    password: 'odkhrs3mroo3nj9h',
-    database: 'q6yopera2c9j4x5e'
-}
-
-module.exports = connection;
